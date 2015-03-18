75f95707160eb8fa20c5ddcdd9f50736105b775f
Jake Buob <jake@mojotech.com>
2015-03-18 00:01:03 -0400

    Add ragtime for migrations

--- project.clj
+++ project.clj
@@ -6,6 +6,10 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/java.jdbc "0.3.2"]
                  [postgresql "9.1-901.jdbc4"]
+                 [ragtime/ragtime.sql.files "0.3.8"]]
   :main ^:skip-aot glug.core
+  :plugins [[ragtime/ragtime.lein "0.3.8"]]
+  :ragtime {:migrations ragtime.sql.files/migrations
+            :database "jdbc:postgresql://localhost:5432/glug"}
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
