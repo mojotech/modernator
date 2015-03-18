f9cac424e66f53b8e341cfd492f16ab75877cce1
Jake Buob <jake@mojotech.com>
2015-03-18 00:00:47 -0400

    Add postgres and jdbc

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://mojotech.com/glug"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/java.jdbc "0.3.2"]
+                 [postgresql "9.1-901.jdbc4"]
   :main ^:skip-aot glug.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
