1917ed0fc6db399207fddc5edda9a9d73b9af320
Jeff Lupinski <lupinskij@gmail.com>
2015-04-13 07:41:33 -0400

    Make it stylish

---
+++ src/assets/README.md
@@ -0,0 +1,17 @@
+Modernator Asset Compiler
+=======
+
+## Installation
+- Install [node.js](http://nodejs.org)
+- `$ npm install -g bower`
+- `$ npm install -g gulp`
+
+## Initial Setup
+
+- `$ cd </folder>/`
+- `$ bower install`
+- `$ npm install`
+
+## Run Dev
+
+- `$ gulp`
---
+++ src/assets/gulpfile.js
@@ -0,0 +1,60 @@
+var gulp            = require('gulp'),
+    stylus          = require('gulp-stylus'),
+    minifyCSS       = require('gulp-minify-css'),
+    autoprefixer    = require('gulp-autoprefixer'),
+    sourcemaps      = require('gulp-sourcemaps'),
+    concat          = require('gulp-concat'),
+    notify          = require('gulp-notify'),
+    livereload      = require('gulp-livereload'),
+    tinylr          = require('tiny-lr'),
+    server          = tinylr(),
+    _if             = require('gulp-if'),
+    isWindows       = /^win/.test(require('os').platform());
+
+// --- Stylus ---
+gulp.task('stylus', function () {
+  gulp.src('./styles/*.styl')
+  .pipe(stylus())
+  .on('error', notify.onError({
+    title: 'Fail',
+    message: 'Stylus error'
+  }))
+  .on('error', function (err) {
+    return console.log(err);
+  })
+    .pipe(sourcemaps.init())
+    .pipe(autoprefixer({
+      browsers: ['last 5 versions']
+    }))
+    .pipe(sourcemaps.write())
+  .pipe(concat("styles.css"))
+  .pipe(minifyCSS('styles.css'))
+  .pipe(gulp.dest('../../resources/public/css'))
+    .pipe(livereload(server))
+    .pipe(_if(!isWindows, notify({
+      title: 'Sucess',
+      message: 'Stylus compiled'
+    })));
+});
+
+// --- Normalize ---
+gulp.task('rename', function() {
+  gulp.src([
+    'bower_components/**/normalize.css'
+  ])
+  .pipe(concat('_vendor.styl'))
+  .pipe(gulp.dest('./styles'));
+});
+
+// --- Watch ---
+gulp.task('watch', function () {
+  server.listen(35729, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    gulp.watch('./styles/*.styl',['stylus']);
+  });
+});
+
+// --- Default task ---
+gulp.task('default', ['stylus', 'rename', 'watch']);
