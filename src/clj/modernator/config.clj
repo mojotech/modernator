53d5b60ee0c102aa423e9a081a6294fd02018560
Seth Krasnianski <sethkrasnianski@gmail.com>
2016-06-09 11:43:56 -0400

    Add default salt value

--- src/clj/modernator/config.clj
+++ src/clj/modernator/config.clj
@@ -2,7 +2,8 @@
   (:require [environ.core :refer [env]]))
 
 (def overrides {:database-url (or (env :database-url) "postgresql://localhost:5432/modernator")
-                :modernator-url (or (env :modernator-url) "http://localhost:8080/")})
+                :modernator-url (or (env :modernator-url) "http://localhost:8080/")
+                :salt (or env :salt) "mojotech"})
 
 (defn config
   "A centralized map for environment variables and their overrides"
